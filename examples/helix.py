01ca10a12ac91429309057739758ccee2fa68dec
Sree Ganesh Balasubramani <sree@pipo.ucsf.edu>
2024-05-07 14:43:31 -0700

    added the latest changes

--- examples/helix.py
+++ examples/helix.py
@@ -10,6 +10,7 @@ import IMP.pmi.topology
 import IMP.pmi.io
 import IMP.pmi.io.crosslink
 import IMP.pmi.restraints
+import IMP.pmi.restraints.saxs
 import IMP.pmi.restraints.crosslinking
 import ihm.cross_linkers
 import IMP.isd
@@ -20,6 +21,7 @@ import IMP.pmi.macros
 import IMP.atom
 import sys
 import IMP.rmf
+import numpy as np
 
 if len(sys.argv) > 1:
     out_dir = sys.argv[1]
@@ -31,6 +33,9 @@ else:
 #------------------------------------------------------------------------------
 pdb_dir = "./data/pdb/"
 fasta_dir = "./data/fasta/"
+saxs_data = "./derived_data/saxs/ala19lys_bb.pdb.dat"
+saxs_weight = 1.0
+
 sequences = IMP.pmi.topology.Sequences(fasta_dir + "ala19lys1.fasta.txt")
 
 mdl = IMP.Model() # Define the model
@@ -49,15 +54,155 @@ hier = sys.build()
 beads = IMP.atom.Selection(hier).get_selected_particles()
 
 IMP.atom.show_with_representations(hier)
+
+#------------------------------------------------------------------------------
+# Define the degrees of freedom and create movers 
+#------------------------------------------------------------------------------
+dof = IMP.pmi.dof.DegreesOfFreedom(mdl)
+lys_rb = alalys[:-2]
+rb1 = dof.create_rigid_body(lys_rb, max_trans=1.0, max_rot=0.5, nonrigid_parts = lys_rb & alalys.get_non_atomic_residues())
+
+#ala1 = dof.create_flexible_beads(alalys.get_non_atomic_residues(), max_trans=1.0)
 #------------------------------------------------------------------------------
 # Add restraints: in this case the data corresponds to some end to end distances
 # as well as some SAXS profiles presumably. The distance restraint is added 
 # as the mean of the distribution obtained from MD simulations. SAXS profile
 # is obtained from the FOXS webserver. Adding contraints on the phi, psi angles
 # would result in formation of a helix, but how do we contrain dihedrals in IMP?
-# 
 #------------------------------------------------------------------------------
-# Use a ideal helix restraint class and check what happens
+
+#------------------------------------------------------------------------------
+# Add restraints for the monomer 
+#------------------------------------------------------------------------------
+# The output_objects list is used to collect all restraints
+# where we want to log the output in the STAT file.
+# Each restraint should be appended to this list.
+output_objects = []
+
+# -----------------------------
+# %%%%% CONNECTIVITY RESTRAINT
+#
+# Restrains residues/particles that are collected in sequence
+# This should be used for any system without an atomic force field
+# (e.g. CHARMM)
+# Here, we pass root_hier to apply this restraint to the entire system
+cr = IMP.pmi.restraints.stereochemistry.ConnectivityRestraint(alalys)
+cr.add_to_model()           # add restraint to the model
+output_objects.append(cr)   # add restraint to the output
+# -----------------------------
+# %%%%% EXCLUDED VOLUME RESTRAINT
+#
+# Keeps particles from occupying the same area in space.
+# Here, we pass a list of both molecule chains to included_objects to
+# apply this to every residue.
+# We could also have passed root_hier to obtain the same behavior.
+#
+# resolution=1000 applies this expensive restraint to the lowest
+# resolution for each particle.
+evr = IMP.pmi.restraints.stereochemistry.ExcludedVolumeSphere(
+                                            included_objects=[alalys],
+                                            resolution=1000)
+output_objects.append(evr)
+# -------------------------
+# %%%%%% Dihedral restraint
+# Just get the dihedrals and print as a first step
+# -------------------------
+# print("residues: ", IMP.atom.Residue(alalys, 1))
+#for r in alalys.residues:
+#    print(r.get_residue_type())
+res = IMP.atom.get_by_type(hier, IMP.atom.RESIDUE_TYPE)
+print("residues: ", res)
+for h in res[1:-2]:
+    r = IMP.atom.Residue(h)
+    phi = IMP.atom.get_phi_dihedral_atoms(r)
+    print("atoms: ", phi[0])
+    d = IMP.core.get_dihedral(*[IMP.core.XYZ(x) for x in phi]) 
+    #print(d)
+    pos = []
+    for i in range(4):
+        pos.append(IMP.core.XYZ(phi[i]))
+    #print(ParticleIndexAdaptor phi[0])
+#    dih_rest = IMP.core.DihedralRestraint(mdl, ParticleIndexAdaptor phi[0], phi[1], phi[2], phi[3])
+#    ouput_objects.append(dih_rest)
+    #print(IMP.atom.get_phi_dihedral_atoms(r))
+    #print(IMP.atom.Residue(r).get_residue_type())
+#print("residues: ", mdl.residues)
+#r = IMP.atom.Residue()
+#phi = IMP.atom.get_phi_dihedral_atoms()
+
+
+# -------------------------
+# %%%%% SAXS RESTRAINT
+#
+# Scores the SAXS data against the predicted SAXS for the input_objects
+# of the model
+#
+# The form factor type (ff_type) for calculating the model SAXS profile
+# can be either:
+#   IMP.saxs.RESIDUES - Residue-level calculation - requires a model
+#                       at resolution=1
+#   IMP.saxs.CA_ATOMS - Residue-level calculation - requires a model
+#                       at atomic resolution
+#   IMP.saxs.HEAVY_ATOMS - No explicit hydrogens - requires a model
+#                          at atomic resolution
+#   IMP.saxs.ALL_ATOMS - Uses explicit hydrogens - requires a model
+#                        at atomic resolution with hydrogens
+sr = IMP.pmi.restraints.saxs.SAXSRestraint(
+    input_objects=[alalys],
+    saxs_datafile=saxs_data,
+    weight=saxs_weight,         # scaling factor for the SAXS score
+    ff_type=IMP.saxs.CA_ATOMS,
+    # Maximum q at which to compare SAXS curves. Defaults are:
+    #   ~0.15 for residue-level calculations
+    #   0.4 for HEAVY_ATOMS
+    #   0.5 for ALL_ATOMS (up to 1.0)
+    maxq=0.15)
+output_objects.append(sr)
+
+#####################################################
+#                      SAMPLING                     #
+#####################################################
+# With our representation and scoring functions determined, we can now sample
+# the configurations of our model with respect to the information.
+
+# First shuffle all particles to randomize the starting point of the
+# system. For larger systems, you may want to increase max_translation
+IMP.pmi.tools.shuffle_configuration(hier,
+                                    max_translation=50)
+
+# Shuffling randomizes the bead positions. It's good to
+# allow these to optimize first to relax large connectivity
+# restraint scores.  100-500 steps is generally sufficient.
+dof.optimize_flexible_beads(500)
+
+evr.add_to_model()
+#emr.add_to_model()
+# slx.add_to_model()
+sr.add_to_model()
+
+# Run replica exchange Monte Carlo sampling
+#rex = IMP.pmi.macros.ReplicaExchange(
+#    mdl,
+#    # pass the root hierarchy
+#    root_hier=hier,
+#    # pass all objects to be moved ( almost always dof.get_movers() )
+#    monte_carlo_sample_objects=dof.get_movers(),
+#    # The output directory for this sampling run.
+#    global_output_directory='run_manual1/output/',
+#    # Items in output_objects write information to the stat file.
+#    output_objects=output_objects,
+#    # Number of MC steps between writing frames
+#    monte_carlo_steps=10,
+#    # set >0 to store best PDB files (but this is slow)
+#    number_of_best_scoring_models=0,
+#    # Total number of frames to run / write to the RMF file.
+#    number_of_frames=200)
+#
+## Ok, now we finally do the sampling!
+#rex.execute_macro()
+
+# Outputs are then analyzed using a separate analysis script.
+
 # hrest = IMP.pmi.restraints.stereochemistry.HelixRestraint(hier, (0,20,alalys)) this didnt work, of course
 
 
--- examples/pmi_restraints.py
+++ examples/pmi_restraints.py
@@ -1,6 +1,103 @@
-
+"""PMI-style restraints."""
+import math
 import IMP
+import IMP.isd
+import IMP.core
+import IMP.container
+import IMP.atom
+import IMP.pmi
+import IMP.pmi.tools
 import IMP.pmi.restraints
 
+class ConnectAtomsRestraint(IMP.pmi.restraints.RestraintBase):
+    """
+    Restraint to keep the sequence of atoms connected because this is necessary to
+    keep the protein structure intact
+    """
+    def __init__(self, 
+                 objects,
+                 scale = 1.0,
+                 disorderedlength=False,
+                 upperharmonic=True,
+                 resolution=0,
+                 label=None):
+        self.m = root_hier.get_model()
+        
+
 # Restraint the dihedrals to form a helix
+class DihedralHelixRestraint(object):
+    """ Restrain a protein using the ideal helix dihedrals 
+        from the Ramachandran plot. In this case the phi angle is 
+    """
+    def __init__(self, tuple_selection_quads, dihedrals, root_hier):
+        self.m = root_hier.get_model()
+        # create restraint sets to join restraints
+        self.rs = IMP.RestraintSet(self.m, "likelihood")
+        self.rs_priors = IMP.RestraintSet(self.m, "priors")
+
+        # create nuisance particles
+        self.kappa = IMP.pmi.tools.SetupNuisance(
+            self.m, 1., 1e-5, 1e5, isoptimized=True).get_particle()
+
+        # create NOE likelihood restraints
+        for (tuple1, tuple2, tuple3, tuple4), angle in zip(
+                tuple_selection_quads, dihedrals):
+            # get atom 1
+            p1 = self.get_atom_from_selection(root_hier, tuple1)
+            p2 = self.get_atom_from_selection(root_hier, tuple2)
+            p3 = self.get_atom_from_selection(root_hier, tuple3)
+            p4 = self.get_atom_from_selection(root_hier, tuple4)
+
+            # create restraint and add to set
+            angle = angle * math.pi / 180.  # convert to radians
+            r = IMP.isd.TALOSRestraint(self.m, p1, p2, p3, p4, [angle],
+                                       self.kappa)
+            r.set_name(
+                "DihedralRestraint_{0}:{1}_{2}:{3}_{4}:{5}_{6}:{7}".format(
+                    tuple1[0], tuple1[1], tuple2[0], tuple2[1],
+                    tuple3[0], tuple3[1], tuple4[0], tuple4[1]))
+            self.rs.add_restraint(r)
+
+        # create prior restraints
+        self.rs_priors.add_restraint(IMP.isd.vonMisesKappaJeffreysRestraint(
+            self.m, self.kappa))
+
+    def get_atom_from_selection(self, root_hier, sel_tuple):
+        res_id, atom_name = sel_tuple
+        sel = IMP.atom.Selection(root_hier,
+                                 resolution=0,
+                                 residue_index=res_id,
+                                 atom_type=IMP.atom.AtomType(atom_name))
+        return sel.get_selected_particles()[0]
+
+    def add_to_model(self):
+        """Add the restraints to the model."""
+        for rs in [self.rs, self.rs_priors]:
+            IMP.pmi.tools.add_restraint_to_model(self.m, rs)
+
+    def get_restraint(self):
+        return self.rs
+
+    def get_restraint_for_rmf(self):
+        """Get the restraint for visualization in an RMF file."""
+        return self.rs
+
+    def get_particles_to_sample(self):
+        """Get any created particles which should be sampled."""
+        out = {}
+        out["Nuisances_Kappa"] = ([self.kappa], .1)
+        return out
+
+    def get_output(self):
+        """Get outputs to write to stat files."""
+        output = {}
+        self.m.update()
+        likelihood_score = self.rs.unprotected_evaluate(None)
+        prior_score = self.rs_priors.unprotected_evaluate(None)
+        output["_TotalScore"] = likelihood_score + prior_score
+        output["DihedralLikelihood_Score"] = likelihood_score
+        output["DihedralPrior_Score"] = prior_score
+        output["Dihedral_Kappa"] = self.kappa.get_scale()
+        return output
+        
 
