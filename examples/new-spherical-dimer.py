976597784c09a718faa9919c1d8f44ee08884c0f
Sree Ganesh Balasubramani <sree@pipo.ucsf.edu>
2024-05-31 16:04:32 -0700

    added code for the dimer structure prediction within IMP

--- examples/new-spherical-dimer.py
+++ examples/new-spherical-dimer.py
@@ -33,15 +33,15 @@ class ParticleSystem:
         # create 4 particles with coordinates (0, 0, 0), (0, 0, 1), (0, 0, 2), (0, 0, 3)
         i = 0
         for _ in range(num_particles):
-            j = random.uniform(-10.0,10.0)
-            k = random.uniform(-10.0,10.0)
-            l = random.uniform(-10.0,10.0)
+            j = random.uniform(-11.0,10.0)
+            k = random.uniform(-11.0,10.0)
+            l = random.uniform(-11.0,10.0)
             p = IMP.Particle(self.model) # create a particle
             self.particles.append(p) # add the particle to the list of particles
             dr = IMP.core.XYZR.setup_particle(p) # create a decorator for the particle
             dr.set_coordinates(IMP.algebra.Vector3D(j, k, l))
-            if i == 0 or i == num_particles - 1:
-                dr.set_radius(2.0)
+            if i == 0 or i == num_particles // 2 - 1 or i == num_particles // 2 or i == num_particles - 1:
+                dr.set_radius(1.8)
             else:
                 dr.set_radius(1.0)
             dr.set_coordinates_are_optimized(True)
@@ -51,23 +51,32 @@ class ParticleSystem:
             hr.add_child(IMP.atom.Hierarchy.setup_particle(dr))
             self.hier.add_child(hr)
             i = i + 1
-        print("particles = : ", self.particles)
         return self.particles
 
     def create_bayesian_restraints(self):
         # create a Bayesian restraint for the particle system
         # create a Jeffreys prior for the distance parameter between two adjacent particles
-        hubound = IMP.core.HarmonicUpperBound(4.8, 10.0)
-        hlbound = IMP.core.HarmonicLowerBound(1.8, 10.0)
-        for i in range(len(self.particles) -1):
+        hubound = IMP.core.HarmonicUpperBound(2.5, 20.0)
+        hlbound = IMP.core.HarmonicLowerBound(1.1, 20.0)
+        for i in range(len(self.particles) // 2 -1):
             r1 = IMP.core.DistanceRestraint(self.model, hubound, self.particles[i], self.particles[i+1])
             r2 = IMP.core.DistanceRestraint(self.model, hlbound, self.particles[i], self.particles[i+1])
-            ambres = IMP.isd.AmbiguousRestraint(self.model, 10.0, r1, r2)
+            ambres = IMP.isd.AmbiguousRestraint(self.model, 20.0, r1, r2)
             self.rs.add_restraint(ambres)
-            #print("ambiguous restraint: ", ambres)
+            r1 = IMP.core.DistanceRestraint(self.model, hubound, self.particles[len(self.particles) - i -1], self.particles[len(self.particles) - i - 2])
+            r2 = IMP.core.DistanceRestraint(self.model, hlbound, self.particles[len(self.particles) - i -1], self.particles[len(self.particles) - i - 2])
+            ambres = IMP.isd.AmbiguousRestraint(self.model, 20.0, r1, r2)
+            self.rs.add_restraint(ambres)
         # adding excluded volume restraint so that the particles do not overlap
-        ev = IMP.core.ExcludedVolumeRestraint(self.particles, 10.0, 1, "EV")
+        ev = IMP.core.ExcludedVolumeRestraint(self.particles, 20.0, 1, "EV")
         self.rs.add_restraint(ev)
+        # Repulsive restraint between particles belonging to the two different strings
+        hubound = IMP.core.HarmonicUpperBound(11.0, 10.0)
+        hlbound = IMP.core.HarmonicLowerBound(6.0, 10.0)
+        for i in range(len(self.particles) // 2):
+            self.rs.add_restraint(IMP.core.DistanceRestraint(self.model, hubound, self.particles[i], self.particles[len(self.particles) // 2 + i]))
+            self.rs.add_restraint(IMP.core.DistanceRestraint(self.model, hlbound, self.particles[i], self.particles[len(self.particles) // 2 + i]))
+            print("connection between particles: ", self.particles[i], self.particles[len(self.particles) // 2 + i])
 
         return self.rs
 
@@ -107,12 +116,10 @@ class ParticleSystem:
         IMP.rmf.add_hierarchy(f, self.hier)
         IMP.rmf.add_restraints(f, [restraints])
         IMP.rmf.add_particles(f, self.particles)
-        #o = IMP.pmi.output.Output()
         os = IMP.rmf.SaveOptimizerState(self.model, f)
         for i in range(10):
             os.update_always("initial conformation")
             os.set_log_level(IMP.SILENT)
-            #os.set_simulator(mc)
             #mc.add_optimizer_state(os)
             mc.optimize(num_steps)
             print("number of accepted MCMC steps: ", mc.get_number_of_accepted_steps())
@@ -126,33 +133,34 @@ class ParticleSystem:
         # The end to end distance of the 10 particle setup is the global parameter
         # Calculate the end to end distance
         dfirst = IMP.core.XYZR(self.model, self.particles[0]).get_coordinates()
+        dlast = IMP.core.XYZR(self.model, self.particles[(len(self.particles)) // 2 - 1]).get_coordinates()
+        end_to_end1 = IMP.algebra.get_distance(dfirst, dlast) # calculate the distance between the first and last particles
+        print(f"end_to_end1 = {end_to_end1} between particles {self.particles[0]} and {self.particles[(len(self.particles)) // 2 -1]}")
+        dfirst = IMP.core.XYZR(self.model, self.particles[(len(self.particles)) // 2 ]).get_coordinates()
         dlast = IMP.core.XYZR(self.model, self.particles[-1]).get_coordinates()
-        end_to_end = IMP.algebra.get_distance(dfirst, dlast) # calculate the distance between the first and last particles
-        return end_to_end
+        end_to_end2 = IMP.algebra.get_distance(dfirst, dlast) # calculate the distance between the first and last particles
+        print(f"end_to_end2 = {end_to_end2} between particles {self.particles[(len(self.particles)) // 2 ]} and {self.particles[-1]}")
+        return end_to_end1, end_to_end2
 
 # Create an instance of the ParticleSystem class
 particle_system = ParticleSystem()
 
 # Create four particles
-particle_system.create_particles(10)
+particle_system.create_particles(20)
 
 # Get the optimized coordinates of the particles
 optimized_coordinates = particle_system.get_optimized_coordinates()
-#print("Initial optimized_coordinates = : ", optimized_coordinates)
 
 # Print the optimized coordinates
 for i, coord in enumerate(optimized_coordinates):
     print(f"Particle {i+1}: {coord}")
 
 # Run the MCMC sampler
-sampler = particle_system.run_mcmc_sampler(200, 2.0)
+sampler = particle_system.run_mcmc_sampler(2000, 2.0)
 
 # Get the optimized coordinates of the particles
 optimized_coordinates = particle_system.get_optimized_coordinates()
-#print("optimized_coordinates = : ", optimized_coordinates)
 # Print the optimized coordinates
 for i, coord in enumerate(optimized_coordinates):
     print(f"Particle {i+1}: {coord}")
-eedis = particle_system.define_global_parameter()
-print("end to end distance as calculated: ", eedis)
-print("hierarchy:", particle_system.hier)
+eedis1, eedis2 = particle_system.define_global_parameter()
--- examples/spherical_dimer.py
+++ examples/spherical_dimer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #------------------------------------------
-# test_spherical_particles.py 
-# create a simple particle system with four particles and run an MCMC sampler
+# spherical_dimer.py 
+# create a simple particle system with a dimer of ten particles 
+# and run an MCMC sampler
 # to optimize their positions. Furthermore, make this simple MC sampler into a
 # two level MCMC sampler with two conformationally distinct states
 #------------------------------------------
@@ -26,19 +27,20 @@ class ParticleSystem:
     def __init__(self):
         self.model = IMP.Model()
         self.hier = IMP.atom.Hierarchy.setup_particle(IMP.Particle(self.model))
-        self.particles = []
+        self.particles1 = []
+        self.particles2 = []
         self.rs = IMP.RestraintSet(self.model, "PairRestraintSet")
 
-    def create_particles(self, num_particles):
-        # create 4 particles with coordinates (0, 0, 0), (0, 0, 1), (0, 0, 2), (0, 0, 3)
+    def string_of_beads(self, num_particles, iter):
+        # create a string of beads with 10 particles
+        pts = []
         i = 0
         for _ in range(num_particles):
             j = random.uniform(-10.0,10.0)
             k = random.uniform(-10.0,10.0)
             l = random.uniform(-10.0,10.0)
-            p = IMP.Particle(self.model) # create a particle
-            self.particles.append(p) # add the particle to the list of particles
-            dr = IMP.core.XYZR.setup_particle(p) # create a decorator for the particle
+            p = IMP.Particle(self.model)
+            dr = IMP.core.XYZR.setup_particle(p)
             dr.set_coordinates(IMP.algebra.Vector3D(j, k, l))
             if i == 0 or i == num_particles - 1:
                 dr.set_radius(2.0)
@@ -46,28 +48,63 @@ class ParticleSystem:
                 dr.set_radius(1.0)
             dr.set_coordinates_are_optimized(True)
             IMP.atom.Mass.setup_particle(p, 0.5)
-            # these are newly added, if the code breaks, comment them out
-            hr = IMP.atom.Hierarchy.setup_particle(IMP.Particle(self.model, "molecule" + str(i)))
+            pts.append(p)
+            hr = IMP.atom.Hierarchy.setup_particle(IMP.Particle(self.model, "molecule" + str(i) + "_iter" + str(iter)))
             hr.add_child(IMP.atom.Hierarchy.setup_particle(dr))
             self.hier.add_child(hr)
             i = i + 1
-        print("particles = : ", self.particles)
-        return self.particles
 
+        return pts
+
+    def create_particles(self, num_particles):
+        # create two sets of particle systems
+        self.particles1 = self.string_of_beads(num_particles, 1)
+        self.particles2 = self.string_of_beads(num_particles, 2)        
+        # create hierarchies for set 1 and set 2
+        #hier_set1 = IMP.atom.Hierarchy.setup_particle(IMP.Particle(self.model, "set1"))
+        #hier_set2 = IMP.atom.Hierarchy.setup_particle(IMP.Particle(self.model, "set2"))
+        
+        #for i in range(num_particles):
+        #    hr1 = IMP.atom.Hierarchy.setup_particle(IMP.Particle(self.model, "molecule" + str(i) + "_set1"))
+        #    hr1.add_child(IMP.atom.Hierarchy.setup_particle(dr1))
+        #    hier_set1.add_child(hr1)
+            
+        #    hr2 = IMP.atom.Hierarchy.setup_particle(IMP.Particle(self.model, "molecule" + str(i) + "_set2"))
+        #    hr2.add_child(IMP.atom.Hierarchy.setup_particle(dr2))
+        #    hier_set2.add_child(hr2)
+        
+        #self.hier.add_child(hier_set1)
+        #self.hier.add_child(hier_set2)
+        #print("hierarchy: ", hier_set1)
+        
+        #print("particles set 1 = : ", self.particles1)
+        #print("particles set 2 = : ", self.particles2)
+        
+        return self.particles1, self.particles2
+    
     def create_bayesian_restraints(self):
         # create a Bayesian restraint for the particle system
         # create a Jeffreys prior for the distance parameter between two adjacent particles
         hubound = IMP.core.HarmonicUpperBound(4.8, 10.0)
         hlbound = IMP.core.HarmonicLowerBound(1.8, 10.0)
-        for i in range(len(self.particles) -1):
-            r1 = IMP.core.DistanceRestraint(self.model, hubound, self.particles[i], self.particles[i+1])
-            r2 = IMP.core.DistanceRestraint(self.model, hlbound, self.particles[i], self.particles[i+1])
-            ambres = IMP.isd.AmbiguousRestraint(self.model, 10.0, r1, r2)
-            self.rs.add_restraint(ambres)
+        for i in range(len(self.particles1) -1):
+            r1 = IMP.core.DistanceRestraint(self.model, hubound, self.particles1[i], self.particles1[i+1])
+            r2 = IMP.core.DistanceRestraint(self.model, hlbound, self.particles1[i], self.particles1[i+1])
+            r3 = IMP.core.DistanceRestraint(self.model, hubound, self.particles2[i], self.particles2[i+1])
+            r4 = IMP.core.DistanceRestraint(self.model, hlbound, self.particles2[i], self.particles2[i+1])
+            ambres1 = IMP.isd.AmbiguousRestraint(self.model, 10.0, r1, r2)
+            self.rs.add_restraint(ambres1)
+            ambres2 = IMP.isd.AmbiguousRestraint(self.model, 10.0, r3, r4)
+            self.rs.add_restraint(ambres2)
             #print("ambiguous restraint: ", ambres)
         # adding excluded volume restraint so that the particles do not overlap
-        ev = IMP.core.ExcludedVolumeRestraint(self.particles, 10.0, 1, "EV")
+        ev = IMP.core.ExcludedVolumeRestraint(self.particles1, 10.0, 1, "EV")
         self.rs.add_restraint(ev)
+        ev = IMP.core.ExcludedVolumeRestraint(self.particles2, 10.0, 1, "EV")
+        self.rs.add_restraint(ev)
+        # add a distance restraint between the middle particles of the two strings
+        self.rs.add_restraint(IMP.core.DistanceRestraint(self.model, hubound, self.particles1[4], self.particles2[4]))
+        self.rs.add_restraint(IMP.core.DistanceRestraint(self.model, hlbound, self.particles1[4], self.particles2[4]))
 
         return self.rs
 
@@ -83,10 +120,12 @@ class ParticleSystem:
         hubound = IMP.core.HarmonicUpperBound(4.0, 10.0)
         hlbound = IMP.core.HarmonicLowerBound(2.0, 10.0)
         for i in range(len(self.particles) - 1):
-            self.rs.add_restraint(IMP.core.DistanceRestraint(self.model, hubound, self.particles[i], self.particles[i+1]))
-            self.rs.add_restraint(IMP.core.DistanceRestraint(self.model, hlbound, self.particles[i], self.particles[i+1]))
+            self.rs.add_restraint(IMP.core.DistanceRestraint(self.model, hubound, self.particles1[i], self.particles1[i+1]))
+            self.rs.add_restraint(IMP.core.DistanceRestraint(self.model, hlbound, self.particles1[i], self.particles1[i+1]))
+            self.rs.add_restraint(IMP.core.DistanceRestraint(self.model, hubound, self.particles2[i], self.particles2[i+1]))
+            self.rs.add_restraint(IMP.core.DistanceRestraint(self.model, hlbound, self.particles2[i], self.particles2[i+1]))
         # adding excluded volume restraint so that the particles do not overlap
-        ev = IMP.core.ExcludedVolumeRestraint(self.particles, 10.0, 1, "EV")
+        ev = IMP.core.ExcludedVolumeRestraint(self.particles1, 10.0, 1, "EV")
         self.rs.add_restraint(ev)
         return self.rs
 
@@ -100,13 +139,14 @@ class ParticleSystem:
         mc.set_log_level(IMP.SILENT)
         mc.set_scoring_function([restraints])
         mc.set_kt(temperature) # this is how temperature is set in MonteCarlo module
-        bmvr = [IMP.core.BallMover(self.model, x, 0.5) for x in self.particles]
-        #mv = IMP.core.SerialMover(bmvr)
+        # add ball movers for two sets of strings of particles
+        bmvr = [IMP.core.BallMover(self.model, x, 0.5) for x in self.particles1 + self.particles2]
         mc.add_movers(bmvr) # add the movers to the MonteCarlo object
         f = RMF.create_rmf_file("balls-move.rmf3")
-        IMP.rmf.add_hierarchy(f, self.hier)
         IMP.rmf.add_restraints(f, [restraints])
-        IMP.rmf.add_particles(f, self.particles)
+        IMP.rmf.add_particles(f, self.particles1 + self.particles2)
+        #IMP.rmf.add_particles(f, self.particles2)
+        IMP.rmf.add_hierarchy(f, self.hier)
         #o = IMP.pmi.output.Output()
         os = IMP.rmf.SaveOptimizerState(self.model, f)
         for i in range(10):
@@ -120,7 +160,7 @@ class ParticleSystem:
         return mc
 
     def get_optimized_coordinates(self):
-        return [IMP.core.XYZR(p).get_coordinates() for p in self.particles]
+        return [IMP.core.XYZR(p).get_coordinates() for p in self.particles1], [IMP.core.XYZR(p).get_coordinates() for p in self.particles2]
     
     def define_global_parameter(self):
         # The end to end distance of the 10 particle setup is the global parameter
@@ -148,11 +188,10 @@ for i, coord in enumerate(optimized_coordinates):
 sampler = particle_system.run_mcmc_sampler(200, 2.0)
 
 # Get the optimized coordinates of the particles
-optimized_coordinates = particle_system.get_optimized_coordinates()
+#optimized_coordinates1, optimized_coordinates2 = particle_system.get_optimized_coordinates()
 #print("optimized_coordinates = : ", optimized_coordinates)
 # Print the optimized coordinates
 for i, coord in enumerate(optimized_coordinates):
     print(f"Particle {i+1}: {coord}")
 eedis = particle_system.define_global_parameter()
 print("end to end distance as calculated: ", eedis)
-print("hierarchy:", particle_system.hier)
