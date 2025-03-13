a047095c86a5af3a644dc315cc11b35570d4811c
Sree Ganesh Balasubramani <sree@pipo.ucsf.edu>
2025-03-13 12:23:10 -0700

    very small changes

--- examples/toy_model/run_samplers.py
+++ examples/toy_model/run_samplers.py
@@ -99,8 +99,8 @@ def main():
             #"run": False,
             "run": True,
             "n_chains": 1,
-            "n_steps": 200000,
-            "save_freq": 100,
+            "n_steps": 300000,
+            "save_freq": 1000,
             "use_sigma_dist": False, # Example of sampler-specific parameter
         },
         "tetramer_sampler": {
