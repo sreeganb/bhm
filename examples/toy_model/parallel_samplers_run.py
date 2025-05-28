5756245a93db78567a882ecc44fd7d3e24a9565f
Sree Ganesh Balasubramani <sreeganeshbalasubramani@Srees-MacBook-Pro-6.local>
2025-05-27 17:11:48 -0700

    fixing merge conflicts

--- examples/toy_model/parallel_samplers_run.py
+++ examples/toy_model/parallel_samplers_run.py
@@ -204,7 +204,7 @@ def main():
     #sampler_sequence = ["pair_sampler", "tetramer_sampler", "pair_sampler"]
     #mcmc_steps = [10000, 5000, 10000]
     sampler_sequence = ["pair_sampler", "tetramer_sampler"]
-    mcmc_steps = [5000000, 1000000]
+    mcmc_steps = [500000, 500000]
     #sampler_sequence = ["tetramer_sampler"]
     #mcmc_steps = [25000]
     #sampler_sequence = ["tetramer_sampler", "octet_sampler"]
